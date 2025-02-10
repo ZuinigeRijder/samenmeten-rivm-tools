d5a3f0a090367f43f08bc7e24ae0fc334bb99d01
Zuinige Rijder <zuinige.rijder@gmail.com>
2025-02-10 10:08:15 +0100

    schrijf madavi in samenmeten csv format

--- madavi_to_samenmeten_format.py
+++ madavi_to_samenmeten_format.py
@@ -11,6 +11,7 @@ import glob
 from os import path
 import logging
 import logging.config
+from pathlib import Path
 import sys
 
 from utils import arg_has, dbg, set_debug, to_float, to_local_datetime
@@ -29,30 +30,34 @@ if arglen == 1 or arglen < 3 or arglen > 4:
     print(
         """
 Usage:
-   python to_samenmeten_rivm_tools_format.py chip-id samenmeten-sensor-name [debug]
+   python to_samenmeten_rivm_tools_format.py chip-id samenmeten-sensor.csv [debug]
 Inputfiles:
    chip-id*.csv
-   samenmeten-sensor-name.csv
+   samenmeten-sensor.csv
 Outputfile:
-   samenmeten-sensor-name.madavi.csv
+   samenmeten-sensor.madavi.csv
 Example:
-   python to_samenmeten_rivm_tools_format.py esp8266-10147413 LTD_54311
+   python to_samenmeten_rivm_tools_format.py esp8266-10147413 LTD_54311.csv
 
 Notes:
 - the script will read all csv files starting with chip-id in the current directory
-- samenmeten-sensor-name.csv is used to get the kalibration factors for PM10 and PM2.5
-  and convert this to a format compatible with samenmeten-rivm-tools
-- datetime,pm10 kal,pm10,pm2.5 kal,pm2.5,rh%,temp,pm10 kal factor,pm2.5 kal factor
-- output is in file samenmeten-sensor-name.madavi.csv
+- samenmeten-sensor.csv is used to get the kalibration factors for PM10 and PM2.5
+- write samenmeten-sensor.madavi.csv with format compatible with samenmeten-rivm-tools
+  datetime,pm10 kal,pm10,pm2.5 kal,pm2.5,rh%,temp,pm10 kal factor,pm2.5 kal factor
+- note that the result contains all madavi data, so not hourly data
+- samenvatting.py takes care of non-hourly data
         """
     )
     sys.exit(-1)
 
 CHIP_ID = sys.argv[1]
-SENSOR_NAME = sys.argv[2]
+SENSOR_CSV = sys.argv[2]
+
+PM25_KAL_FACTOR_DICT: dict[str, float] = {}
+PM10_KAL_FACTOR_DICT: dict[str, float] = {}
 
 
-def process_csv_file(csvfile: str):
+def process_csv_file(csvfile: str, result: list[str]):
     """process_csv_file"""
     _ = D and dbg(f"Processing: {csvfile}")
     with open(csvfile, encoding="utf-8") as f:
@@ -65,24 +70,60 @@ def process_csv_file(csvfile: str):
                 splitted = line.split(";")
                 if len(splitted) > 8:
                     dt = to_local_datetime(splitted[0].replace("/", "-"))
+                    timestamp = dt.strftime("%Y-%m-%d %H")
+                    pm10_kal_factor = PM10_KAL_FACTOR_DICT.get(timestamp, 1.0)
+                    pm25_kal_factor = PM25_KAL_FACTOR_DICT.get(timestamp, 1.0)
                     pm10 = to_float(splitted[7])  # column SDS_P1
                     pm25 = to_float(splitted[8])  # column SDS_P2
                     temp = to_float(splitted[15])  # column BME280_temperature
                     rh = to_float(splitted[16])  # column BME280_humidity
-                    print(
-                        f"{dt},pm10 kal,{pm10},pm2.5 kal,{pm25},{rh},{temp},pm10 kal factor,pm2.5 kal factor"  # noqa
+                    pm10_kal = pm10 * pm10_kal_factor
+                    pm25_kal = pm25 * pm25_kal_factor
+                    result.append(
+                        f"{dt},{pm10_kal:.2f},{pm10},{pm25_kal:.2f},{pm25},{rh},{temp},{pm10_kal_factor},{pm25_kal_factor}"  # noqa
                     )
+
+
+def read_kalibration():
+    """read kalibration"""
+    with open(SENSOR_CSV, encoding="utf-8") as f:
+        count = 0
+        for line in f:
+            line = line.strip()
+            count += 1
+            _ = D and dbg(f"{count}: {line}")
+            if count > 1:
+                splitted = line.split(",")
+                if len(splitted) == 9:
+                    timestamp = splitted[0][0:13]
+                    pm10_kal_factor = to_float(splitted[7])
+                    pm25_kal_factor = to_float(splitted[8])
+                    if D:
+                        print(
+                            f"timestamp: {timestamp}, {pm10_kal_factor}, {pm25_kal_factor}"  # noqa
+                        )
+                    PM10_KAL_FACTOR_DICT[timestamp] = pm10_kal_factor
+                    PM25_KAL_FACTOR_DICT[timestamp] = pm25_kal_factor
 
 
 def get_csv_files():
     """get_csv_files"""
     filelist = glob.glob(f"data-{CHIP_ID}*.csv")
-    if len(filelist) > 0:
-        print(
-            "datetime,pm10 kal,pm10,pm2.5 kal,pm2.5,rh%,temp,pm10 kal factor,pm2.5 kal factor"  # noqa
-        )
+    result: list[str] = []
     for csvfile in sorted(filelist):
-        process_csv_file(csvfile)
+        process_csv_file(csvfile, result)
+
+    result.sort()
+    outputfile = Path(SENSOR_CSV).stem
+    outputfile = f"{outputfile}.madavi.csv"
+    print(f"Writing: {outputfile}")
+    with open(outputfile, "w", encoding="utf-8") as f:
+        f.write(
+            "datetime,pm10 kal,pm10,pm2.5 kal,pm2.5,rh%,temp,pm10 kal factor,pm2.5 kal factor\n"  # noqa
+        )
+        for line in result:
+            f.write(f"{line}\n")
 
 
+read_kalibration()
 get_csv_files()
